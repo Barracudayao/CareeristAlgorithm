9fb63e18a499c4b57e041ceca102575ef816fae3
Barracudakun <yingxukun@yahoo.com>
2021-10-19 20:30:52 -0500

    delete

--- AlgorithmHW1/AlgorithmHW1_2.py
+++
@@ -1,6 +0,0 @@
-# Find max number from 3 values, entered manually from a keyboard.
-# Example: 124, 21, 32. Result = 124.
-
-
-
-
--- AlgorithmHW1/AlgorithmHW1_3.py
+++
@@ -1,2 +0,0 @@
-# Count odd and even numbers. Count odd and even digits of the whole number.
-# Example: entered number is 34560, then 3 digits will be even (4, 6, and 0) and 2 odd digits (3 and 5).
